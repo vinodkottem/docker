8413f6e5e562d56496697259bd23e573d6e208f1
vinodkottem <vinodkottem@gmail.com>
2017-07-26 21:46:34 +0530

    DOCKER IMAGE FOR MY SQL ADDED

---
+++ mysql-docker/setup.sql
@@ -0,0 +1,6 @@
+create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email TEXT, phone_number TEXT);
+insert into users (email, phone_number) values ('vinod', '1234567890');
+insert into users (email, phone_number) values ('kumar', '2234567890');
+insert into users (email, phone_number) values ('naveen', '3234567890');
+insert into users (email, phone_number) values ('mahesh', '4234567890');
+insert into users (email, phone_number) values ('pratap', '5234567890');
